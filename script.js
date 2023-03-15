6bf7ea950c638d86fe95f50be5cd9ef54a580e63
Madranges Lucas <lucasmadranges06@gmail.com>
2023-03-15 22:02:47 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const button1 = document.querySelector('#button1');
+const button2 = document.querySelector('#button2');
+
+button1.addEventListener('click', Event => {window.getElementById('TOTAL').textContent = '1'});
