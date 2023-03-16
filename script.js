2db121021c2b3139c80a8f39221ec5f6aa90c3be
Madranges Lucas <lucasmadranges06@gmail.com>
2023-03-16 15:25:58 +0100

    Avancement du script de la calculatrice, ajout des boutons d'opérations et amélioration de l'affichage de la calculatrice

--- script.js
+++ script.js
@@ -1,4 +1,63 @@
-const button1 = document.querySelector('#button1');
-const button2 = document.querySelector('#button2');
+function calcul(number)
+{
+    var texte;
+    switch(number){
+        case "0":
+            texte = "0";
+            break;
+        case "1":
+            texte = "1";
+            break;
+        case "2":
+            texte = "2";
+            break;
+        case "3":
+            texte = "3";
+            break;
+        case "4":
+            texte = "4";
+            break;
+        case "5":
+            texte = "5";
+            break;
+        case "6":
+            texte = "6";
+            break;
+        case "7":
+            texte = "7";
+            break;
+        case "8":
+            texte = "8";
+            break;
+        case "9":
+            texte = "9";
+            break;
+        case "ENTER":
+            
+            break;
+        case "DEL":
+            texte = document.getElementById('TOTAL').textContent = "";
+            break;
+        case "+":
+            texte = " + ";
+            break;
+        case "-":
+            texte = " - ";
+            break;
+        case "x":
+            texte = " x ";
+            break;
+        case "/":
+            texte = " / ";
+            break;
+    }
+    var calcul = document.getElementById('TOTAL').textContent += texte;
+}
 
-button1.addEventListener('click', Event => {window.getElementById('TOTAL').textContent = '1'});
+const button = document.querySelectorAll('button');
+button.forEach(item => {
+    item.addEventListener('click',() => {
+        let number = item.textContent;
+        calcul(number);
+    });
+});
